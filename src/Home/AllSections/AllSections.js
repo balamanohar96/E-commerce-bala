1675c4d8f57ff48626d6ca61a3295619f3e4ee64
bala manohar <balamanohar.yeminedi@gmail.com>
2024-07-23 18:27:21 +0530

    duplicate comp removed

--- src/Home/AllSections/AllSections.js
+++ src/Home/AllSections/AllSections.js
@@ -29,25 +29,6 @@ const AllSections = () => {
         </div>
       </div>
       <StayUpdated />
-      <div className="pt-5">
-        <h2 className="decorated">
-          <span>Just Arrived</span>
-        </h2>
-      </div>
-      <div className="container-fluid trendy-shop-container">
-        <div className="row">
-          {TrendyProductsData.map((each) => {
-            return (
-              <div
-                key={each.id}
-                className="col-lg-4 col-xl-3 col-md-6 col-sm-12"
-              >
-                <TrendySection key={each.id} shopDetails={each} />
-              </div>
-            );
-          })}
-        </div>
-      </div>
     </>
   );
 };
